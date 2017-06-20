df56414c4ef925301e1520495d1c475d3384d65b
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-06-20 12:24:19 -0700

    Create Loading component in Loading.js

---
+++ app/components/Loading.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import React from 'react';
+
+class Loading extends React.Component {
+  render() {
+    return (
+      <h3>Loading</h3>
+    )
+  }
+}
+
+export default Loading;
--- app/components/Table.js
+++ app/components/Table.js
@@ -2,39 +2,55 @@
 
 import React from 'react';
 import api from '../utils/api';
+import Loading from './Loading';
 
 class Table extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      category: 'recent'
+      category: '',
+      loading: true,
+      data: []
     }
+    // this.componentDidMount = this.componentDidMount.bind(this);
   }
   componentDidMount() {
-    var category = this.state.category;
-    const endpoint = `https://fcctop100.herokuapp.com/api/fccusers/top/${category}`;
+    const endpoint = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
     api.fetchAPI(endpoint)
-      .then(function(data){
+      .then( (data) => {
         console.log(data);
+        this.setState(function(){
+          return {
+            category: 'recent',
+            loading: false,
+            data: data
+          }
+        })
       })
   }
   render() {
+    var loading = this.state.loading;
     return (
-      <table>
-        <thead>
+      <div>
+        <table>
+          <thead>
+             <tr>
+               <th>Leaderboard</th>
+             </tr>
+           </thead>
+           <tbody>
            <tr>
-             <th>Leaderboard</th>
+             <td>#</td>
+             <td>Camper Name</td>
+             <td>Points in past 30 days</td>
+             <td>All time points</td>
            </tr>
-         </thead>
-         <tbody>
-         <tr>
-           <td>#</td>
-           <td>Camper Name</td>
-           <td>Points in past 30 days</td>
-           <td>All time points</td>
-         </tr>
-         </tbody>
-      </table>
+           </tbody>
+        </table>
+
+        {loading && <Loading />}
+        
+      </div>
     )
   }
 }
