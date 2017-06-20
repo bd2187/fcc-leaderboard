04e53b95b0520366380f17a28aee5155ded603dd
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-06-20 11:24:56 -0700

    First commit. Set up package.json and install modules.

---
+++ app/components/Table.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import React from 'react';
+
+class Table extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Table</h1>
+      </div>
+    )
+  }
+}
+
+export default Table;
