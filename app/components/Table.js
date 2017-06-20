f15b787d1b57a07aaf34d82e5912517834a26f58
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-06-20 11:49:36 -0700

    Set up table

--- app/components/Table.js
+++ app/components/Table.js
@@ -3,11 +3,29 @@
 import React from 'react';
 
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    console.log('fetch data');
+  }
   render() {
     return (
-      <div>
-        <h1>Table</h1>
-      </div>
+      <table>
+        <thead>
+           <tr>
+             <th>Leaderboard</th>
+           </tr>
+         </thead>
+         <tbody>
+         <tr>
+           <td>#</td>
+           <td>Camper Name</td>
+           <td>Points in past 30 days</td>
+           <td>All time points</td>
+         </tr>
+         </tbody>
+      </table>
     )
   }
 }
